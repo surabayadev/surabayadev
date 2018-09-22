620efd26353ee1b23316c500d3f055496410afe1
Antoni Putra <akiddcode@gmail.com>
2018-09-22 16:31:36 +0700

    update event google form

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -5,7 +5,7 @@
 	<div class="container content">
 		<div class="row">
 			<div class="col-md-10 col-md-offset-1">
-				<iframe src="https://docs.google.com/forms/d/e/1FAIpQLScXV7Ehzc7sH-oWpZqImSA-CuE12_MfA97bXxf8K5e2Ra1C5Q/viewform" style="width: 100%; height: 1300px; border: none;">Memuat...</iframe>
+				<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd0LM1ZDs28ffADuc_Y5tdrtCo_wOGDERORuh3kSPN5eKs27w/viewform?usp=pp_url" style="width: 100%; height: 1300px; border: none;">Memuat...</iframe>
 			</div>
 		</div>
 	</div>
