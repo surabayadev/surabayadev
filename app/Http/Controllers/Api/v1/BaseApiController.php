06b0f0f75ade94c7b495e477eb32b80cb7e19a04
Nadhif <nadhifhayazee@gmail.com>
2019-01-05 16:28:45 +0700

    fix conflict

--- app/Http/Controllers/Api/v1/BaseApiController.php
+++ app/Http/Controllers/Api/v1/BaseApiController.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller;
 
-abstract class BaseApiControlleraaa extends Controller
+abstract class BaseApiController extends Controller
 {
     use DispatchesJobs, ValidatesRequests;
 }
